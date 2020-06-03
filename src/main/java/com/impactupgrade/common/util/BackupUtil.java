29bc99dfc906510ae5921fe754c8fd25726adc40
Brett Meyer <brett@3riverdev.com>
2020-06-03 11:56:15 -0400

    BackupUtil and genericized/simplified salesforce-backup.rb

---
+++ src/main/java/com/impactupgrade/common/util/BackupUtil.java
@@ -0,0 +1,97 @@
+package com.impactupgrade.common.util;
+
+import com.backblaze.b2.client.B2StorageClient;
+import com.backblaze.b2.client.B2StorageClientFactory;
+import com.backblaze.b2.client.contentSources.B2ContentSource;
+import com.backblaze.b2.client.contentSources.B2ContentTypes;
+import com.backblaze.b2.client.contentSources.B2FileContentSource;
+import com.backblaze.b2.client.structures.B2FileVersion;
+import com.backblaze.b2.client.structures.B2UploadFileRequest;
+import com.backblaze.b2.client.structures.B2UploadListener;
+import com.backblaze.b2.util.B2ExecutorUtils;
+import org.apache.commons.io.FileUtils;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.jruby.embed.PathType;
+import org.jruby.embed.ScriptingContainer;
+
+import java.io.File;
+import java.util.Optional;
+import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
+
+public class BackupUtil {
+
+  private static final Logger log = LogManager.getLogger(BackupUtil.class);
+
+  private static final String BACKBLAZE_KEYID = System.getenv("BACKBLAZE.KEYID");
+  private static final String BACKBLAZE_KEY = System.getenv("BACKBLAZE.KEY");
+  private static final String BACKBLAZE_BUCKETID = System.getenv("BACKBLAZE.BUCKETID");
+
+  /**
+   * Backup SFDC using a mix of https://github.com/carojkov/salesforce-export-downloader and
+   * https://github.com/Backblaze/b2-sdk-java.
+   */
+  public static void backupSFDC(String sfdcUsername, String sfdcPassword, String sfdcUrl) {
+    // some of the tasks (like Backblaze B2) can multi-thread and process in parallel, so create
+    // an executor pool for the whole setup to run off of
+    final ExecutorService executorService = Executors.newFixedThreadPool(
+        10, B2ExecutorUtils.createThreadFactory("backup-executor-%02d"));
+
+    Runnable thread = () -> {
+      try {
+        // start with a clean slate and delete the dir used by the script
+        FileUtils.deleteDirectory(new File("backup-salesforce"));
+
+        log.info("downloading backup file from SFDC");
+
+        // using jruby to kick off the ruby script -- see https://github.com/carojkov/salesforce-export-downloader
+        ScriptingContainer container = new ScriptingContainer();
+        container.getEnvironment().put("SFDC_USERNAME", sfdcUsername);
+        container.getEnvironment().put("SFDC_PASSWORD", sfdcPassword);
+        container.getEnvironment().put("SFDC_URL", sfdcUrl);
+        container.runScriptlet(PathType.CLASSPATH, "salesforce-export-downloader/salesforce-backup.rb");
+
+        // should have only downloaded a single zip, so grab the first file
+        Optional<File> f = FileUtils.listFiles(new File("backup-salesforce"), null, false)
+            .stream().findFirst();
+
+        if (f.isPresent()) {
+          log.info("uploading {} to Backblaze B2", f.get().getName());
+
+          // from here on out, closely following example code from
+          // https://github.com/Backblaze/b2-sdk-java/blob/master/samples/src/main/java/com/backblaze/b2/sample/B2Sample.java
+
+          B2StorageClient client = B2StorageClientFactory
+              .createDefaultFactory()
+              .create(BACKBLAZE_KEYID, BACKBLAZE_KEY, "impact-upgrade-hub");
+          B2ContentSource source = B2FileContentSource
+              .builder(f.get())
+              .build();
+
+          final B2UploadListener uploadListener = (progress) -> {
+            final double percent = (100. * (progress.getBytesSoFar() / (double) progress.getLength()));
+            log.info(String.format("upload progress: %3.2f, %s", percent, progress.toString()));
+          };
+
+          B2UploadFileRequest request = B2UploadFileRequest
+              .builder(BACKBLAZE_BUCKETID, "salesforce/" + f.get().getName(), B2ContentTypes.APPLICATION_OCTET, source)
+              .setListener(uploadListener)
+              .build();
+          B2FileVersion upload = client.uploadLargeFile(request, executorService);
+
+          log.info("upload complete: {}", upload);
+        } else {
+          log.warn("could not find the downloaded SFDC backup file");
+        }
+      } catch (Exception e) {
+        log.error("SFDC backup failed", e);
+      }
+    };
+    new Thread(thread).start();
+  }
+
+  public static void main(String[] args) {
+    backupSFDC("support@tinyhandsintl.org", "}CanXq!m8<c9KVAD4j5GbZwjSeNgclDz0znFFFw1u", "lovejustice.my.salesforce.com");
+  }
+}
---
+++ src/main/resources/salesforce-export-downloader/salesforce-backup.rb
@@ -0,0 +1,205 @@
+#!/usr/bin/ruby
+
+require 'net/http'
+require 'net/https'
+require 'rexml/document'
+require 'date'
+require 'net/smtp'
+require 'yaml'
+require 'fileutils'
+
+include REXML
+
+class Result
+  def initialize(xmldoc)
+    @xmldoc = xmldoc
+  end
+
+  def server_url
+    @server_url ||= XPath.first(@xmldoc, '//result/serverUrl/text()')
+  end
+
+  def session_id
+    @session_id ||= XPath.first(@xmldoc, '//result/sessionId/text()')
+  end
+
+  def org_id
+    @org_id ||= XPath.first(@xmldoc, '//result/userInfo/organizationId/text()')
+  end
+end
+
+class SfError < Exception
+  attr_accessor :resp
+
+  def initialize(resp)
+    @resp = resp
+  end
+
+  def inspect
+    puts resp.body
+  end
+  alias_method :to_s, :inspect
+end
+
+### Helpers ###
+
+def http(host=ENV["SFDC_URL"], port=443)
+    h = Net::HTTP.new(host, port)
+    h.use_ssl = true
+    h
+end
+
+def headers(login)
+  {
+    'Cookie'         => "oid=#{login.org_id.value}; sid=#{login.session_id.value}",
+    'X-SFDC-Session' => login.session_id.value
+  }
+end
+
+def file_name(url=nil)
+  datestamp = Date::today.strftime('%Y-%m-%d')
+  uid_string = url ? "-#{/.*fileName=(.*)\.ZIP.*/.match(url)[1]}" : ''
+  "salesforce-#{datestamp}#{uid_string}.ZIP"
+end
+
+def progress_percentage(current, total)
+  ((current.to_f/total.to_f)*(100.to_f)).to_i
+end
+
+### Salesforce interactions ###
+
+def login
+  puts "Logging in..."
+  path = '/services/Soap/u/28.0'
+
+  pwd_token_encoded = ENV["SFDC_PASSWORD"]
+  pwd_token_encoded = pwd_token_encoded.gsub(/&(?!amp;)/,'&amp;')
+  pwd_token_encoded = pwd_token_encoded.gsub(/</,'&lt;')
+  pwd_token_encoded = pwd_token_encoded.gsub(/>/,'&gt;')
+
+  inital_data = <<-EOF
+<?xml version="1.0" encoding="utf-8" ?>
+<env:Envelope xmlns:xsd="http://www.w3.org/2001/XMLSchema"
+    xmlns:xsi="http://www.w3.org/2001/XMLSchema-instance"
+    xmlns:env="http://schemas.xmlsoap.org/soap/envelope/">
+  <env:Body>
+    <n1:login xmlns:n1="urn:partner.soap.sforce.com">
+      <n1:username>#{ENV["SFDC_USERNAME"]}</n1:username>
+      <n1:password>#{pwd_token_encoded}</n1:password>
+    </n1:login>
+  </env:Body>
+</env:Envelope>
+  EOF
+
+  puts inital_data
+
+  initial_headers = {
+    'Content-Type' => 'text/xml; charset=UTF-8',
+    'SOAPAction' => 'login'
+  }
+
+  resp = http('login.salesforce.com').post(path, inital_data, initial_headers)
+
+  if resp.code == '200'
+    xmldoc = Document.new(resp.body)
+    return Result.new(xmldoc)
+  else
+    raise SfError.new(resp)
+  end
+end
+
+def download_index(login)
+  puts "Downloading index..."
+  path = '/servlet/servlet.OrgExport'
+  data = http.post(path, nil, headers(login))
+  data.body.strip
+end
+
+def get_download_size(login, url)
+  puts "Getting download size..."
+  data = http.head(url, headers(login))
+  data['Content-Length'].to_i
+end
+
+def download_file(login, url, expected_size)
+  printing_interval = 10
+  interval_type = :percentage
+  last_printed_value = nil
+  size = 0
+  fn = file_name(url)
+  puts "Downloading #{fn}..."
+  f = open("backup-salesforce/#{fn}", "wb")
+  begin
+    http.request_get(url, headers(login)) do |resp|
+      resp.read_body do |segment|
+        f.write(segment)
+        size = size + segment.size
+        last_printed_value = print_progress(size, expected_size, printing_interval, last_printed_value, interval_type)
+      end
+      puts "\nFinished downloading #{fn}!"
+    end
+  ensure
+    f.close()
+  end
+  raise "Size didn't match. Expected: #{expected_size} Actual: #{size}" unless size == expected_size
+end
+
+def print_progress(size, expected_size, interval, previous_printed_interval, interval_type=:seconds)
+  percent_file_complete = ((size.to_f/expected_size.to_f)*(100.to_f)).to_i
+  case interval_type
+    when :percentage
+    previous_printed_interval ||= 0
+    current_value = percent_file_complete
+    when :seconds
+    previous_printed_interval ||= Time.now.to_i
+    current_value = Time.now.to_i
+  end
+  next_interval = previous_printed_interval + interval
+  if current_value >= next_interval
+    timestamp = Time.now.strftime('%Y-%m-%d-%H-%M-%S')
+    puts "#{timestamp}: #{percent_file_complete}% complete (#{size} of #{expected_size})"
+    return next_interval
+  end
+  return previous_printed_interval
+end
+
+begin
+  result = login
+  urls = download_index(result).split("\n")
+  puts "  All urls:"
+  puts urls
+  puts ''
+
+  unless File.directory?("backup-salesforce")
+    FileUtils.mkdir_p("backup-salesforce")
+  end
+
+  urls.each do |url|
+    fn = file_name(url)
+    file_path = "backup-salesforce/#{fn}"
+    retry_count = 0
+    begin
+      puts "Working on: #{url}"
+      expected_size = get_download_size(result, url)
+      puts "Expected size: #{expected_size}"
+      fs = File.size?(file_path)
+      if fs && fs == expected_size
+        puts "File #{fn} exists and is the right size. Skipping."
+      else
+        download_file(result, url, expected_size)
+      end
+    rescue Exception => e
+      if retry_count < 5
+        retry_count += 1
+        puts "Error: #{e}"
+        puts "Retrying (retry_count of 5)..."
+        retry
+      end
+    end
+  end
+  puts "Done!"
+end
+
+
+
+
