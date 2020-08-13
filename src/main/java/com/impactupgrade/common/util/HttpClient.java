2953ffb649169a5cef9a0eaac4087179dc61a546
Brett Meyer <brett@3riverdev.com>
2020-08-13 19:48:48 -0400

    beefed up HttpClient

--- src/main/java/com/impactupgrade/common/util/HttpClient.java
+++ src/main/java/com/impactupgrade/common/util/HttpClient.java
@@ -5,8 +5,10 @@ import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.Entity;
 import javax.ws.rs.client.Invocation;
 import javax.ws.rs.client.WebTarget;
+import javax.ws.rs.core.Form;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.util.Map;
 
 public class HttpClient {
 
@@ -18,18 +20,30 @@ public class HttpClient {
     return response.readEntity(String.class);
   }
 
-  public static <T> Response postXml(T entity, String url, String... paths) {
-    return post(MediaType.APPLICATION_XML, entity, url, paths);
+  public static <T> Response postJson(T entity, String bearerToken, String url, String... paths) {
+    return post(MediaType.APPLICATION_JSON, entity, bearerToken, url, paths);
   }
 
-  private static <T> Response post(String mediaType, T entity, String url, String... paths) {
+  public static <T> Response postXml(T entity, String bearerToken, String url, String... paths) {
+    return post(MediaType.APPLICATION_XML, entity, bearerToken, url, paths);
+  }
+
+  public static Response postForm(Map<String, String> data, String bearerToken, String url, String... paths) {
+    Form form = new Form();
+    data.forEach(form::param);
+    return post(MediaType.APPLICATION_FORM_URLENCODED, form, bearerToken, url, paths);
+  }
+
+  private static <T> Response post(String mediaType, T entity, String bearerToken, String url, String... paths) {
     Client client = ClientBuilder.newClient();
     WebTarget webTarget = client.target(url);
     for (String path : paths) {
       webTarget = webTarget.path(path);
     }
 
-    Invocation.Builder invocationBuilder =  webTarget.request(mediaType);
-    return invocationBuilder.put(Entity.entity(entity, mediaType));
+    return webTarget
+        .request(mediaType)
+        .header("Authorization", "Bearer " + bearerToken)
+        .post(Entity.entity(entity, mediaType));
   }
 }
