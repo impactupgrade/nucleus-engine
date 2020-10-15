f0a5ac571ad670c1707f99ec32b03e7ec3af9fd3
Brett Meyer <brett@3riverdev.com>
2020-10-15 14:14:57 -0400

    some additional SFDC queries, 2.1.1.Final

--- src/main/java/com/impactupgrade/common/sfdc/SFDCClient.java
+++ src/main/java/com/impactupgrade/common/sfdc/SFDCClient.java
@@ -51,7 +51,7 @@ public class SFDCClient extends SFDCPartnerAPIClient {
   // ACCOUNTS
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  private static final String ACCOUNT_FIELDS = "id, OwnerId, name, email__c, phone, npo02__NumberOfClosedOpps__c, npo02__TotalOppAmount__c";
+  protected static final String ACCOUNT_FIELDS = "id, OwnerId, name, email__c, phone, npo02__NumberOfClosedOpps__c, npo02__TotalOppAmount__c";
   // TODO: For now, keep this simple and allow apps to statically set custom fields to include. But eventually,
   // this should be config driven!
   public static String CUSTOM_ACCOUNT_FIELDS = "";
@@ -63,10 +63,45 @@ public class SFDCClient extends SFDCPartnerAPIClient {
   }
 
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+  // CAMPAIGNS
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+  protected static final String CAMPAIGN_FIELDS = "id, name";
+  // TODO: For now, keep this simple and allow apps to statically set custom fields to include. But eventually,
+  // this should be config driven!
+  public static String CUSTOM_CAMPAIGN_FIELDS = "";
+  public static String CUSTOM_DEFAULT_CAMPAIGN_ID = "";
+
+  public Optional<SObject> getCampaignById(String campaignId) throws ConnectionException, InterruptedException {
+    String query = "select " + getFieldsList(CONTACT_FIELDS, CUSTOM_CONTACT_FIELDS) + " from campaign where id = '" + campaignId + "'";
+    LoggingUtil.verbose(log, query);
+    return querySingle(query);
+  }
+
+  public SObject getCampaignByIdOrDefault(String campaignId) throws ConnectionException, InterruptedException {
+    return getCampaignById(campaignId).orElseGet(() -> {
+      if (!Strings.isNullOrEmpty(CUSTOM_DEFAULT_CAMPAIGN_ID)) {
+        try {
+          Optional<SObject> defaultCampaign = getCampaignById(CUSTOM_DEFAULT_CAMPAIGN_ID);
+          if (defaultCampaign.isPresent()) {
+            return defaultCampaign.get();
+          }
+        } catch(ConnectionException | InterruptedException e){
+          log.error("failed to retrieve the default campaign", e);
+        }
+      }
+
+      log.warn("SFDC is missing a default campaign");
+      // TODO: this will cause NPEs downstream, but we should treat this as a catastrophic blocker anyway...
+      return null;
+    });
+  }
+
+  //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
   // CONTACTS
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  private static final String CONTACT_FIELDS = "Id, AccountId, OwnerId, FirstName, LastName, account.name, account.BillingStreet, account.BillingCity, account.BillingPostalCode, account.BillingState, account.BillingStateCode, account.BillingCountry, account.BillingCountryCode, name, phone, email, npe01__Home_Address__c, mailingstreet, mailingcity, mailingstate, mailingpostalcode, mailingcountry, homephone, mobilephone, npe01__workphone__c, npe01__preferredphone__c";
+  protected static final String CONTACT_FIELDS = "Id, AccountId, OwnerId, FirstName, LastName, account.name, account.BillingStreet, account.BillingCity, account.BillingPostalCode, account.BillingState, account.BillingStateCode, account.BillingCountry, account.BillingCountryCode, name, phone, email, npe01__Home_Address__c, mailingstreet, mailingcity, mailingstate, mailingpostalcode, mailingcountry, homephone, mobilephone, npe01__workphone__c, npe01__preferredphone__c";
   // TODO: For now, keep this simple and allow apps to statically set custom fields to include. But eventually,
   // this should be config driven!
   public static String CUSTOM_CONTACT_FIELDS = "";
@@ -200,13 +235,25 @@ public class SFDCClient extends SFDCPartnerAPIClient {
   // DONATIONS
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  private static final String DONATION_FIELDS = "id, AccountId, amount, name, RecordTypeId, Campaign_Name__c, CloseDate, StageName, Type, npe03__Recurring_Donation__c";
+  protected static final String DONATION_FIELDS = "id, AccountId, amount, name, RecordTypeId, Campaign_Name__c, CloseDate, StageName, Type, npe03__Recurring_Donation__c";
   // TODO: For now, keep this simple and allow apps to statically set custom fields to include. But eventually,
   // this should be config driven!
   public static String CUSTOM_DONATION_FIELDS = "";
 
+  public Optional<SObject> getDonationById(String donationId) throws ConnectionException, InterruptedException {
+    String query = "select " + getFieldsList(DONATION_FIELDS, CUSTOM_DONATION_FIELDS) + " from Opportunity where id = '" + donationId + "'";
+    LoggingUtil.verbose(log, query);
+    return querySingle(query);
+  }
+
+  public Optional<SObject> getDonationByTransactionId(String transactionIdField, String transactionId) throws ConnectionException, InterruptedException {
+    String query = "select " + getFieldsList(DONATION_FIELDS, CUSTOM_DONATION_FIELDS) + " from Opportunity where " + transactionIdField + " = '" + transactionId + "'";
+    LoggingUtil.verbose(log, query);
+    return querySingle(query);
+  }
+
   public List<SObject> getDonationsByAccountId(String accountId) throws ConnectionException, InterruptedException {
-    String query = "select " + getFieldsList(CONTACT_FIELDS, CUSTOM_CONTACT_FIELDS) + " from Opportunity where accountid = '" + accountId + "' AND StageName != 'Pledged' ORDER BY CloseDate DESC";
+    String query = "select " + getFieldsList(DONATION_FIELDS, CUSTOM_DONATION_FIELDS) + " from Opportunity where accountid = '" + accountId + "' AND StageName != 'Pledged' ORDER BY CloseDate DESC";
     LoggingUtil.verbose(log, query);
     return queryList(query);
   }
@@ -219,6 +266,13 @@ public class SFDCClient extends SFDCPartnerAPIClient {
 
   public Optional<SObject> getLatestPostedDonation(String recurringDonationId) throws ConnectionException, InterruptedException {
     String query = "select " + getFieldsList(DONATION_FIELDS, CUSTOM_DONATION_FIELDS) + " from Opportunity where npe03__Recurring_Donation__c = '" + recurringDonationId + "' and stageName = 'Posted' order by CloseDate desc limit 1";
+    LoggingUtil.verbose(log, query);
+    return querySingle(query);
+  }
+
+  public Optional<SObject> getNextPledgedDonationByRecurringDonationId(String recurringDonationId) throws ConnectionException, InterruptedException {
+    // TODO: query from the original LJI code -- looks okish, but find ways to improve it?
+    String query = "select id, name, amount, CloseDate, AccountId, npe03__Recurring_Donation__c, StageName, campaignid, Type from Opportunity where npe03__Recurring_Donation__c = '" + recurringDonationId + "' AND stageName = 'Pledged' AND CloseDate <= TODAY ORDER BY CloseDate Desc LIMIT 1";
     LoggingUtil.verbose(log, query);
     return querySingle(query);
   }
@@ -237,7 +291,7 @@ public class SFDCClient extends SFDCPartnerAPIClient {
   // RECURRING DONATIONS
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  private static final String RECURRINGDONATION_FIELDS = "id, name, npe03__Recurring_Donation_Campaign__c, npe03__Recurring_Donation_Campaign__r.Name, npe03__Next_Payment_Date__c, npe03__Installment_Period__c, npe03__Amount__c, npe03__Open_Ended_Status__c, npe03__Contact__c, npsp__InstallmentFrequency__c";
+  protected static final String RECURRINGDONATION_FIELDS = "id, name, npe03__Recurring_Donation_Campaign__c, npe03__Recurring_Donation_Campaign__r.Name, npe03__Next_Payment_Date__c, npe03__Installment_Period__c, npe03__Amount__c, npe03__Open_Ended_Status__c, npe03__Contact__c, npsp__InstallmentFrequency__c";
   // TODO: For now, keep this simple and allow apps to statically set custom fields to include. But eventually,
   // this should be config driven!
   public static String CUSTOM_RECURRINGDONATION_FIELDS = "";
@@ -246,6 +300,12 @@ public class SFDCClient extends SFDCPartnerAPIClient {
     String query = "select " + getFieldsList(RECURRINGDONATION_FIELDS, CUSTOM_RECURRINGDONATION_FIELDS) + " from npe03__Recurring_Donation__c where id='" + id + "'";
     LoggingUtil.verbose(log, query);
     return querySingle(query);
+  }
+
+  public List<SObject> getRecurringDonationsBySubscriptionId(String subscriptionIdFieldName, String subscriptionId) throws ConnectionException, InterruptedException {
+    String query = "select id, " + subscriptionIdFieldName + ", npe03__Open_Ended_Status__c, npe03__Amount__c, Type__c, npe03__Recurring_Donation_Campaign__c, npe03__Installment_Period__c from npe03__Recurring_Donation__c where " + subscriptionIdFieldName + " = '" + subscriptionId + "'";
+    LoggingUtil.verbose(log, query);
+    return queryList(query);
   }
 
   public List<SObject> getRecurringDonationsByAccountId(String accountId) throws ConnectionException, InterruptedException {
@@ -258,7 +318,7 @@ public class SFDCClient extends SFDCPartnerAPIClient {
   // USERS
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  private static final String USER_FIELDS = "id, firstName, lastName, email, phone";
+  protected static final String USER_FIELDS = "id, firstName, lastName, email, phone";
   // TODO: For now, keep this simple and allow apps to statically set custom fields to include. But eventually,
   // this should be config driven!
   public static String CUSTOM_USER_FIELDS = "";
@@ -301,7 +361,7 @@ public class SFDCClient extends SFDCPartnerAPIClient {
   // INTERNAL
   //////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
-  private String getFieldsList(String list, String customList) {
+  protected String getFieldsList(String list, String customList) {
     return customList.length() > 0 ? list + ", " + customList : list;
   }
 }
