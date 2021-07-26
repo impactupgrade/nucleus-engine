8a71f1f4c94ef845ff868c598afedeec6ff015de
Brett Meyer <brett@3riverdev.com>
2021-07-26 13:45:15 -0400

    removing cancel-donation endpoint, in favor of the new donation management methods

--- src/main/java/com/impactupgrade/nucleus/controller/PaymentGatewayController.java
+++ src/main/java/com/impactupgrade/nucleus/controller/PaymentGatewayController.java
@@ -15,7 +15,6 @@ import org.apache.logging.log4j.Logger;
 import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.BeanParam;
 import javax.ws.rs.Consumes;
-import javax.ws.rs.FormParam;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
@@ -33,30 +32,6 @@ public class PaymentGatewayController {
 
   public PaymentGatewayController(EnvironmentFactory envFactory) {
     this.envFactory = envFactory;
-  }
-
-  // TODO: Rethink this method. I'm not excited about getRecurringDonation having to return the gateway-specific
-  // subscriptionId -- might be better to instead have the UI/caller pass that info to us...
-  @Path("/cancelrecurringdonation")
-  @POST
-  @Consumes(MediaType.APPLICATION_FORM_URLENCODED)
-  public Response cancelRecurringDonation(@FormParam("recurringDonationId") String recurringDonationId,
-      @Context HttpServletRequest request) throws Exception {
-    SecurityUtil.verifyApiKey(request);
-
-//    Optional<CrmRecurringDonation> recurringDonation = crmSourceService.getRecurringDonation(recurringDonationId);
-//    if (recurringDonation.isPresent()) {
-//      // TODO: Need to make check and see if the RD actually exists first!
-//      // TODO: But I'm not a huge fan of this approach. Think through a better pattern that knows the specific gateway up-front.
-//      for (PaymentGatewayService paymentGatewayService : paymentGatewayServices) {
-//        paymentGatewayService.cancelSubscription(
-//            recurringDonation.get().paymentGatewaySubscriptionId(), recurringDonation.get().paymentGatewayCustomerId());
-//      }
-      return Response.ok().build();
-//    } else {
-//      log.warn("{} was not found in the source CRM", recurringDonationId);
-//      return Response.status(Response.Status.NOT_FOUND).build();
-//    }
   }
 
   @Path("/update-recurring-donation")
