42109633d796fd568701986b3b7bb7dba7162d84
Brett Meyer <brett@3riverdev.com>
2021-12-27 17:34:00 -0500

    catch exceptions in MetricUtil

--- src/main/java/com/impactupgrade/nucleus/util/MetricUtil.java
+++ src/main/java/com/impactupgrade/nucleus/util/MetricUtil.java
@@ -1,12 +1,21 @@
 package com.impactupgrade.nucleus.util;
 
 import com.newrelic.api.agent.NewRelic;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 
 import java.util.Map;
 
 public class MetricUtil {
 
+  private static final Logger log = LogManager.getLogger(MetricUtil.class);
+
   public static void event(String type, Map<String, Object> attributes) {
-    NewRelic.getAgent().getInsights().recordCustomEvent(type, attributes);
+    // Never allow these to bubble up and screw with real processing!
+    try {
+      NewRelic.getAgent().getInsights().recordCustomEvent(type, attributes);
+    } catch (Exception e) {
+      log.info("failed to record NR event", e);
+    }
   }
 }
