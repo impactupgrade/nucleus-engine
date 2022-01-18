4a66f4542b6708deb224bd37bc2ef54d629a2ce5
Brett Meyer <brett@3riverdev.com>
2022-01-17 21:45:31 -0500

    SmtpEmailService, support HS transactional email

--- src/main/java/com/impactupgrade/nucleus/service/segment/HubSpotEmailService.java
+++ src/main/java/com/impactupgrade/nucleus/service/segment/HubSpotEmailService.java
@@ -6,7 +6,7 @@ import org.apache.logging.log4j.Logger;
 
 import java.util.Calendar;
 
-public class HubSpotEmailService implements EmailService {
+public class HubSpotEmailService extends SmtpEmailService {
 
   private static final Logger log = LogManager.getLogger(HubSpotEmailService.class);
 
@@ -23,11 +23,6 @@ public class HubSpotEmailService implements EmailService {
   }
 
   @Override
-  public void sendEmailText(String subject, String body, boolean isHtml, String to, String from) {
-    // TODO
-  }
-
-  @Override
   public void sendEmailTemplate(String template, String to) {
     // TODO
   }
@@ -38,4 +33,9 @@ public class HubSpotEmailService implements EmailService {
     // TODO: May not need this (break up the interface?). If HS used for email, orgs will always have that as their
     //  CRM as well. No syncs ever needed?
   }
+
+  protected String host() { return "smtp.hubapi.com"; }
+  protected String port() { return "587"; }
+  protected String username() { return env.getConfig().hubspotEmail.username; }
+  protected String password() { return env.getConfig().hubspotEmail.password; }
 }
--- src/main/java/com/impactupgrade/nucleus/service/segment/SendGridEmailService.java
+++ src/main/java/com/impactupgrade/nucleus/service/segment/SendGridEmailService.java
@@ -1,34 +1,15 @@
 package com.impactupgrade.nucleus.service.segment;
 
+import com.google.common.base.Strings;
 import com.impactupgrade.nucleus.environment.Environment;
-import com.sun.mail.smtp.SMTPTransport;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
-import javax.mail.Message;
-import javax.mail.MessagingException;
-import javax.mail.Session;
-import javax.mail.internet.InternetAddress;
-import javax.mail.internet.MimeMessage;
 import java.util.Calendar;
-import java.util.Date;
-import java.util.Properties;
 
-public class SendGridEmailService implements EmailService {
+public class SendGridEmailService extends SmtpEmailService {
 
   private static final Logger log = LogManager.getLogger(SendGridEmailService.class);
-
-  // TODO: move to env.json? Or use that as an override and continue to maintain a default instance for clients?
-  private static final String SMTP_SERVER = "smtp.sendgrid.net";
-  private static final String SMTP_USERNAME = "apikey";
-  private static final String SMTP_PASSWORD = System.getenv("SENDGRID_KEY");
-  private static final Properties props = new Properties();
-  static {
-    props.put("mail.smtp.host", "smtp.sendgrid.net");
-    props.put("mail.smtp.auth", "true");
-    props.put("mail.smtp.port", "587");
-    props.put("mail.smtp.timeout", "60000");
-  }
 
   protected Environment env;
 
@@ -43,32 +24,6 @@ public class SendGridEmailService implements EmailService {
   }
 
   @Override
-  public void sendEmailText(String subject, String body, boolean isHtml, String to, String from) {
-    log.info("sendEmailText: to={} from={} subject={} isHtml={}", to, from, subject, isHtml);
-    try {
-      Session session = Session.getInstance(props, null);
-
-      Message msg = new MimeMessage(session);
-      msg.setFrom(new InternetAddress(from));
-      msg.setRecipients(Message.RecipientType.TO, InternetAddress.parse(to, false));
-      msg.setSubject(subject);
-      if (isHtml) {
-        msg.setContent(body, "text/html");
-      } else {
-        msg.setText(body);
-      }
-      msg.setSentDate(new Date());
-
-      SMTPTransport t = (SMTPTransport) session.getTransport("smtp");
-      t.connect(SMTP_SERVER, SMTP_USERNAME, SMTP_PASSWORD);
-      t.sendMessage(msg, msg.getAllRecipients());
-      t.close();
-    } catch (MessagingException e) {
-      log.warn("email failed", e);
-    }
-  }
-
-  @Override
   public void sendEmailTemplate(String template, String to) {
     log.info("not implemented: sendEmailTemplate");
     // TODO
@@ -78,5 +33,16 @@ public class SendGridEmailService implements EmailService {
   public void syncContacts(Calendar lastSync) throws Exception {
     log.info("not implemented: syncContacts");
     // TODO
+  }
+
+  protected String host() { return "smtp.sendgrid.net"; }
+  protected String port() { return "587"; }
+  protected String username() { return "apikey"; }
+  protected String password() {
+    if (Strings.isNullOrEmpty(env.getConfig().sendgridEmail.password)) {
+      // legacy support
+      return System.getenv("SENDGRID_KEY");
+    }
+    return env.getConfig().sendgridEmail.password;
   }
 }
