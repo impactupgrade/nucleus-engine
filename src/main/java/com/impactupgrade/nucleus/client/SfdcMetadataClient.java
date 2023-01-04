c97cd6f5d483eba8c2c38df081dd051927f312a0
Levi Lehman <levilehman16@gmail.com>
2023-01-04 14:48:27 -0500

    Pr Comment Fixes WIP

--- src/main/java/com/impactupgrade/nucleus/client/SfdcMetadataClient.java
+++ src/main/java/com/impactupgrade/nucleus/client/SfdcMetadataClient.java
@@ -36,7 +36,6 @@ import com.sforce.ws.ConnectorConfig;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
-import org.checkerframework.checker.units.qual.C;
 
 import java.util.ArrayList;
 import java.util.Arrays;
