42cc64e77607b9246d2776aee8bb576105a57d1f
vsydor <vs.nr.ua@gmail.com>
2023-04-03 22:22:12 +0300

    Formatting date/time in /jobs response

--- src/main/java/com/impactupgrade/nucleus/controller/JobController.java
+++ src/main/java/com/impactupgrade/nucleus/controller/JobController.java
@@ -6,6 +6,7 @@ import com.impactupgrade.nucleus.entity.Job;
 import com.impactupgrade.nucleus.environment.Environment;
 import com.impactupgrade.nucleus.environment.EnvironmentFactory;
 import com.impactupgrade.nucleus.security.SecurityUtil;
+import com.impactupgrade.nucleus.util.Utils;
 import org.apache.commons.collections.CollectionUtils;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
@@ -22,10 +23,9 @@ import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import java.time.Duration;
-import java.time.LocalDate;
 import java.time.LocalDateTime;
-import java.time.LocalTime;
 import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
@@ -34,6 +34,9 @@ import java.util.stream.Collectors;
 public class JobController {
 
   private static final Logger log = LogManager.getLogger(JobController.class.getName());
+
+  private static final String DATE_FORMAT = "MM-dd-yyyy";
+  private static final String TIME_FORMAT = "HH:mm";
 
   protected final EnvironmentFactory envFactory;
 
@@ -98,8 +101,10 @@ public class JobController {
 
   private List<JobDto> toJobDtos(List<Job> jobs, String timezoneId) {
     String zoneId = !Strings.isNullOrEmpty(timezoneId) ? timezoneId : "EST";
+    DateTimeFormatter dateFormatter = DateTimeFormatter.ofPattern(DATE_FORMAT);
+    DateTimeFormatter timeFormatter = DateTimeFormatter.ofPattern(TIME_FORMAT);
     return jobs.stream()
-        .map(job -> toJobDto(job, zoneId))
+        .map(job -> toJobDto(job, zoneId, dateFormatter, timeFormatter))
         .collect(Collectors.toList());
   }
 
@@ -112,32 +117,34 @@ public class JobController {
 
   private static final class JobDto {
     public String traceId;
-    public LocalDate date;
-    public LocalTime time;
+    public String date;
+    public String time;
     public String platform;
     public String task;
     public String status;
     public String user;
-    public LocalDateTime started;
-    public LocalDateTime ended;
-    public Duration runtime;
+    public String started;
+    public String ended;
+    public String runtime;
   }
 
-  private JobDto toJobDto(Job job, String timezoneId) {
+  private JobDto toJobDto(Job job, String timezoneId, DateTimeFormatter dateFormatter, DateTimeFormatter timeFormatter) {
     JobDto jobDto = new JobDto();
     jobDto.traceId = job.traceId;
     ZoneId zoneId = ZoneId.of(timezoneId);
     LocalDateTime started = LocalDateTime.ofInstant(job.startedAt, zoneId);
-    jobDto.date = started.toLocalDate();
-    jobDto.time = started.toLocalTime();
+    jobDto.date = dateFormatter.format(started.toLocalDate()) + " " + timezoneId;
+    jobDto.time = timeFormatter.format(started.toLocalTime()) + " " + timezoneId;
     jobDto.platform = job.originatingPlatform;
     jobDto.task = job.jobName;
     jobDto.status = job.status.name();
     jobDto.user = job.startedBy;
-    jobDto.started = started;
+    jobDto.started = timeFormatter.format(started) + " " + timezoneId;
     if (job.endedAt != null) {
-      jobDto.ended = LocalDateTime.ofInstant(job.endedAt, zoneId);
-      jobDto.runtime = Duration.between(jobDto.started, jobDto.ended);
+      LocalDateTime ended = LocalDateTime.ofInstant(job.endedAt, zoneId);
+      jobDto.ended = timeFormatter.format(ended) + " " + timezoneId;
+      Duration runtime = Duration.between(started, ended);
+      jobDto.runtime = Utils.formatDuration(runtime);
     }
     return jobDto;
   }
