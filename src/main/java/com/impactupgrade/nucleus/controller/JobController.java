184b769dddb04e8273ef54520559eaa984f566d4
Levi Lehman <levilehman16@gmail.com>
2023-06-15 16:13:42 -0400

    Removed unused imports

--- src/main/java/com/impactupgrade/nucleus/controller/JobController.java
+++ src/main/java/com/impactupgrade/nucleus/controller/JobController.java
@@ -26,7 +26,6 @@ import java.time.LocalDateTime;
 import java.time.ZoneId;
 import java.time.format.DateTimeFormatter;
 import java.util.Collections;
-import java.util.Comparator;
 import java.util.List;
 import java.util.stream.Collectors;
 
