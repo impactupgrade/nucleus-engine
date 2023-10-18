2877172461a3e2f463fabbd9fe0b67e5efdf43d3
Brett Meyer <brett@3riverdev.com>
2023-10-18 16:43:04 -0400

    WIP: RaisersEdgeToSalesforceHOUSEHOLDFIXES

--- src/main/java/com/impactupgrade/nucleus/util/RaisersEdgeToSalesforceHOUSEHOLDFIXES.java
+++ src/main/java/com/impactupgrade/nucleus/util/RaisersEdgeToSalesforceHOUSEHOLDFIXES.java
@@ -8,7 +8,6 @@ import com.google.common.base.Strings;
 import com.impactupgrade.nucleus.client.SfdcClient;
 import com.impactupgrade.nucleus.environment.Environment;
 import com.impactupgrade.nucleus.environment.EnvironmentConfig;
-import com.impactupgrade.nucleus.model.CrmImportEvent;
 import com.sforce.soap.partner.sobject.SObject;
 import com.sforce.ws.bind.XmlObject;
 import org.apache.poi.util.IOUtils;
@@ -67,16 +66,14 @@ public class RaisersEdgeToSalesforceHOUSEHOLDFIXES {
     sfdcClient.queryListAutoPaged("SELECT Id, FirstName, LastName, Blackbaud_Constituent_ID__c, Account.Blackbaud_Constituent_ID__c FROM Contact WHERE Blackbaud_Constituent_ID__c!=''")
         .forEach(c -> existingContactsByExtRef.put(c.getField("Blackbaud_Constituent_ID__c").toString(), c));
 
-    List<Map<String, String>> primaryRows = new ArrayList<>();
-
     // TODO: There's unfortunately a wide, inconsistent mix of ways that households can be defined in RE data.
     //  Relationships between spouses will usually identify one as the head-of-household, so we first use those heads
     //  as the primary contacts of accounts. But then students/children do *not* have a parent relationship that's
     //  defined as HoH, so we can't globally rely on that to give away household groupings. We instead opt to use
     //  mailing addresses, since RE at least makes these consistent (at least for CLHS).
     List<String> headOfHouseholdIds = new ArrayList<>();
-    Map<String, String> spouseIdsToHouseholdIds = new HashMap<>();
-    Map<String, String> addressesToHouseholdIds = new HashMap<>();
+    Map<String, String> addressesToHouseholdId = new HashMap<>();
+    Map<String, List<String>> addressesToConstituentIds = new HashMap<>();
 
     // Make one pass to discover all heads of households. This is unfortunately not at the Constituent level, but is
     // instead buried in the lists of relationships :(
@@ -94,8 +91,6 @@ public class RaisersEdgeToSalesforceHOUSEHOLDFIXES {
           // the relationship is the head of household, not this constituent itself
           String headOfHouseholdId = row.get(prefix + "ID");
           headOfHouseholdIds.add(headOfHouseholdId);
-          // since we have the direct relationship, might as well save it off and use it later
-          spouseIdsToHouseholdIds.put(id, headOfHouseholdId);
           break;
         }
       }
@@ -104,46 +99,49 @@ public class RaisersEdgeToSalesforceHOUSEHOLDFIXES {
     // Make another pass to discover the heads of households' addresses.
     for (Map<String, String> row : rows) {
       String id = row.get("CnBio_ID");
-      if (headOfHouseholdIds.contains(id)) {
-        Set<String> cnAdrPrfAddr = new LinkedHashSet<>();
-        for (int i = 1; i <= 4; i++) {
-          String field = "CnAdrPrf_Addrline" + i;
-          if (!Strings.isNullOrEmpty(row.get(field))) {
-            cnAdrPrfAddr.add(row.get(field));
-          }
-        }
-        String street = String.join(", ", cnAdrPrfAddr);
-        String zip = row.get("CnAdrPrf_ZIP");
-        String address = street + " " + zip;
-        address = address.trim();
-        if (!Strings.isNullOrEmpty(address) && !addressesToHouseholdIds.containsKey(address)) {
-          addressesToHouseholdIds.put(address, id);
+
+      Set<String> cnAdrPrfAddr = new LinkedHashSet<>();
+      for (int i = 1; i <= 4; i++) {
+        String field = "CnAdrPrf_Addrline" + i;
+        if (!Strings.isNullOrEmpty(row.get(field))) {
+          cnAdrPrfAddr.add(row.get(field));
         }
       }
-    }
+      String street = String.join(", ", cnAdrPrfAddr);
+      String zip = row.get("CnAdrPrf_ZIP");
+      String address = street + " " + zip;
+      address = address.trim();
 
-    // TODO: If any addresses do NOT have a HoH, should we make another pass and specifically look for parents,
-    //  guardians, or grandparents first? Prevent kids from becomings heads of households?
-
-    // The next pass inserts all constituents that were discovered to be heads of households, ensuring that they're
-    // the primary contact on households as they're created.
-    for (Map<String, String> row : rows) {
-      String id = row.get("CnBio_ID");
       if (headOfHouseholdIds.contains(id)) {
-        // We use the head of household's ID as the account's extref key.
-        String householdId = id;
-        migrateConstituent(row, householdId, primaryRows, existingAccountsByExtRef, existingContactsByExtRef);
+        if (!Strings.isNullOrEmpty(address) && !addressesToHouseholdId.containsKey(address)) {
+          addressesToHouseholdId.put(address, id);
+        }
       }
+
+      if (!addressesToConstituentIds.containsKey(address)) {
+        addressesToConstituentIds.put(address, new ArrayList<>());
+      }
+      addressesToConstituentIds.get(address).add(id);
     }
 
-    List<CrmImportEvent> importEvents = CrmImportEvent.fromGeneric(primaryRows);
-    env.primaryCrmService().processBulkImport(importEvents);
+//    for (Map<String, String> row : rows) {
+//      String id = row.get("CnBio_ID");
+//      if (headOfHouseholdIds.contains(id) && existingAccountsByExtRef.containsKey(id) && existingContactsByExtRef.containsKey(id)) {
+//        SObject account = new SObject("Account");
+//        account.setId(existingAccountsByExtRef.get(id).getId());
+//        account.setField("npe01__One2OneContact__c", existingContactsByExtRef.get(id).getId());
+//        sfdcClient.batchUpdate(account);
+//      }
+//    }
+//    sfdcClient.batchFlush();
 
-    primaryRows.clear();
-
-    // Then do another pass to insert everyone else. Doing this after the above ensure households already exist.
     for (Map<String, String> row : rows) {
       String id = row.get("CnBio_ID");
+
+      if (!id.equalsIgnoreCase("47080")) {
+        continue;
+      }
+
       if (!headOfHouseholdIds.contains(id)) {
         Set<String> cnAdrPrfAddr = new LinkedHashSet<>();
         for (int i = 1; i <= 4; i++) {
@@ -157,24 +155,28 @@ public class RaisersEdgeToSalesforceHOUSEHOLDFIXES {
         String address = street + " " + zip;
         address = address.trim();
 
-        String householdId;
-        if (spouseIdsToHouseholdIds.containsKey(id)) {
-          householdId = spouseIdsToHouseholdIds.get(id);
-        } else if (!Strings.isNullOrEmpty(address) && addressesToHouseholdIds.containsKey(address)) {
-          householdId = addressesToHouseholdIds.get(address);
-        } else {
-          // business or single-contact household (IE, no defined HoH) -- use their ID for the household extref
-          householdId = id;
-        }
+        if (existingContactsByExtRef.get(id) != null) {
+          if (Strings.isNullOrEmpty(address)) {
 
-        migrateConstituent(row, householdId, primaryRows, existingAccountsByExtRef, existingContactsByExtRef);
+          } else if (addressesToHouseholdId.get(address) != null) {
+//            if (existingAccountsByExtRef.get(addressesToHouseholdIds.get(address)) != null) {
+//              SObject contact = new SObject("Contact");
+//              contact.setId(existingContactsByExtRef.get(id).getId());
+//              contact.setField("AccountId", existingAccountsByExtRef.get(addressesToHouseholdIds.get(address)).getId());
+//              sfdcClient.batchUpdate(contact);
+//            } else {
+//              System.out.println("MISSING HOUSEHOLD " + id);
+//            }
+          } else if (addressesToConstituentIds.get(address) != null && addressesToConstituentIds.get(address).size() == 1) {
+            // only one contact in the house, so they'll be primary already
+          }
+          else {
+            System.out.println(id + " (" + row.get("CnBio_First_Name") + " " + row.get("CnBio_Last_Name")+ ")");
+          }
+        }
       }
     }
-
-    importEvents = CrmImportEvent.fromGeneric(primaryRows);
-    env.primaryCrmService().processBulkImport(importEvents);
-
-    primaryRows.clear();
+    sfdcClient.batchFlush();
   }
 
   private static void migrateConstituent(Map<String, String> row, String accountExtRef, List<Map<String, String>> primaryRows,
