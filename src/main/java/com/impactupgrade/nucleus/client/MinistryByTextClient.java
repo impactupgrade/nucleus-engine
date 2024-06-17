d9080c246a5ed3e2e43bc0611ec76fbce70c65c8
VSydor <vs.nr.ua@gmail.com>
2024-06-17 23:35:47 +0300

    15859: mbt bulk endpoints to ceate subscribers and contacts

--- src/main/java/com/impactupgrade/nucleus/client/MinistryByTextClient.java
+++ src/main/java/com/impactupgrade/nucleus/client/MinistryByTextClient.java
@@ -17,6 +17,7 @@ import javax.ws.rs.core.Response;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static com.impactupgrade.nucleus.util.HttpClient.get;
 import static com.impactupgrade.nucleus.util.HttpClient.isOk;
@@ -25,10 +26,25 @@ import static javax.ws.rs.core.MediaType.APPLICATION_JSON;
 
 public class MinistryByTextClient extends OAuthClient {
 
-  protected static String AUTH_ENDPOINT = "https://login.ministrybytext.com/connect/token";
-  protected static String API_ENDPOINT_BASE = "https://api.ministrybytext.com/";
+  protected static String AUTH_URL_PRODUCTION = "https://login.ministrybytext.com/connect/token";
+  protected static String AUTH_URL_SANDBOX = "https://login-qa.poweredbytext.com/connect/token";
+  protected static String API_BASE_URL_PRODUCTION = "https://api.ministrybytext.com";
+  protected static String API_BASE_URL_SANDBOX = "https://api-qa.poweredbytext.com";
 
   protected final EnvironmentConfig.MBT mbtConfig;
+
+  protected static final String AUTH_URL;
+  protected static final String API_BASE_URL;
+  static {
+    String profile = System.getenv("PROFILE");
+    if ("production".equalsIgnoreCase(profile)) {
+      AUTH_URL = AUTH_URL_PRODUCTION;
+      API_BASE_URL = API_BASE_URL_PRODUCTION;
+    } else {
+      AUTH_URL = AUTH_URL_SANDBOX;
+      API_BASE_URL = API_BASE_URL_SANDBOX;
+    }
+  }
 
   public MinistryByTextClient(EnvironmentConfig.MBT mbtConfig, Environment env) {
     super("ministrybytext", env);
@@ -49,25 +65,40 @@ public class MinistryByTextClient extends OAuthClient {
 
   @Override
   protected OAuthContext oAuthContext() {
-    return new ClientCredentialsOAuthContext(mbtConfig, AUTH_ENDPOINT, false);
+    return new ClientCredentialsOAuthContext(mbtConfig, AUTH_URL, false);
   }
 
   public List<Group> getGroups(EnvironmentConfig.MBT mbtConfig) {
-    return get(API_ENDPOINT_BASE + "campuses/" + mbtConfig.campusId + "/groups", headers(), new GenericType<>() {});
+    return get(API_BASE_URL + "campuses/" + mbtConfig.campusId + "/groups", headers(), new GenericType<>() {});
   }
 
   public Subscriber upsertSubscriber(CrmContact crmContact, EnvironmentConfig.MBT mbtConfig, EnvironmentConfig.CommunicationList communicationList) {
     Subscriber subscriber = toMBTSubscriber(crmContact);
-    return post(API_ENDPOINT_BASE + "campuses/" + mbtConfig.campusId + "/groups/" + communicationList.id + "/subscribers", subscriber, APPLICATION_JSON, headers(), Subscriber.class);
+    return post(API_BASE_URL + "campuses/" + mbtConfig.campusId + "/groups/" + communicationList.id + "/subscribers", subscriber, APPLICATION_JSON, headers(), Subscriber.class);
+  }
+
+  public BulkOperationResponse upsertSubscribersBulk(String orgunitId, String groupId, List<CrmContact> crmContacts) {
+    List<Subscriber> subscribers = crmContacts.stream()
+            .map(this::toMBTSubscriber)
+            .collect(Collectors.toList());
+    return post(API_BASE_URL + "/orgunit/" + orgunitId + "/groups/" + groupId + "/new-subscribers/bulk",
+            subscribers, APPLICATION_JSON, headers(), BulkOperationResponse.class);
   }
 
   public void upsertNotificationSetting(NotificationSetting notificationSetting,
       EnvironmentConfig.MBT mbtConfig, EnvironmentConfig.CommunicationList communicationList) throws IOException, InterruptedException {
-    patch(API_ENDPOINT_BASE + "campuses/" + mbtConfig.campusId + "/groups/" + communicationList.id + "/notification-url", notificationSetting, APPLICATION_JSON, headers());
+    patch(API_BASE_URL + "campuses/" + mbtConfig.campusId + "/groups/" + communicationList.id + "/notification-url", notificationSetting, APPLICATION_JSON, headers());
   }
 
   public NotificationSettingResponse getNotificationSetting(EnvironmentConfig.MBT mbtConfig, EnvironmentConfig.CommunicationList communicationList) throws IOException, InterruptedException {
-    return get(API_ENDPOINT_BASE + "campuses/" + mbtConfig.campusId + "/groups/" + communicationList.id + "/notification-url", headers(), new GenericType<>() {});
+    return get(API_BASE_URL + "campuses/" + mbtConfig.campusId + "/groups/" + communicationList.id + "/notification-url", headers(), new GenericType<>() {});
+  }
+
+  public BulkOperationResponse upsertContactsBulk(String orgunitId, List<CrmContact> crmContacts) {
+    List<Contact> contacts = crmContacts.stream()
+            .map(this::toMBTContact)
+            .collect(Collectors.toList());
+    return post(API_BASE_URL + "/orgunit/" + orgunitId + "/contacts/bulk", contacts, APPLICATION_JSON, headers(), BulkOperationResponse.class);
   }
 
   // Having to modify this due to MBT's limited API. There's no upsert concept, and we want to avoid having to retrieve
@@ -97,6 +128,17 @@ public class MinistryByTextClient extends OAuthClient {
     // TODO: relations
 
     return subscriber;
+  }
+
+  protected Contact toMBTContact(CrmContact crmContact) {
+    Contact contact = new Contact();
+    contact.firstName = crmContact.firstName;
+    contact.lastName = crmContact.lastName;
+    //contact.gender = crmContact ?
+    contact.mobileNo = crmContact.mobilePhone;
+    // TODO: address, using mailing or billing
+    //contact.region = crmContact ?
+    return contact;
   }
 
   @JsonIgnoreProperties(ignoreUnknown = true)
@@ -189,5 +231,78 @@ public class MinistryByTextClient extends OAuthClient {
           "data=" + data +
           '}';
     }
+  }
+
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public static class Contact {
+    public String firstName;
+    public String lastName;
+    public String gender;
+    public String mobileNo;
+    public SubscriberAddress address;
+    public String region;
+  }
+
+  @JsonIgnoreProperties(ignoreUnknown = true)
+  public static class BulkOperationResponse {
+    public String message;
+    public boolean isError;
+    public String appCode;
+    public Data data;
+
+    @Override
+    public String toString() {
+      return "BulkOperationResponse{" +
+              "message='" + message + '\'' +
+              ", isError=" + isError +
+              ", appCode='" + appCode + '\'' +
+              ", data=" + data +
+              '}';
+    }
+  }
+
+  public static class Data {
+    public String batchId;
+
+    @Override
+    public String toString() {
+      return "Data{" +
+              "batchId='" + batchId + '\'' +
+              '}';
+    }
+  }
+
+  public static void main(String[] args) {
+    Environment env = new Environment() {
+      @Override
+      public EnvironmentConfig getConfig() {
+
+        EnvironmentConfig.MBT mbt = new EnvironmentConfig.MBT();
+        mbt.clientId = "GVU05RE7VMACNUU96ME8";
+        mbt.clientSecret = "CPMw462MQdLTW6MDDlagUWlOhxXXJgDRc0D8cBHuUqhO=g6ELo";
+        mbt.campusId = "cf774a3b-4910-4b16-b6b0-608f80d216a4";
+
+        EnvironmentConfig envConfig = new EnvironmentConfig();
+        envConfig.ministrybytext = List.of(mbt);
+        return envConfig;
+      }
+    };
+    EnvironmentConfig.MBT mbt = env.getConfig().ministrybytext.get(0);
+    MinistryByTextClient mbtClient = new MinistryByTextClient(mbt, env);
+
+    CrmContact crmContact = new CrmContact();
+    crmContact.id = "12345";
+    crmContact.firstName = "Brett";
+    crmContact.lastName = "Meyer";
+    crmContact.mobilePhone = "260-349-5732";
+    EnvironmentConfig.CommunicationList communicationList = new EnvironmentConfig.CommunicationList();
+    communicationList.id = "c64ecadf-bbfa-4cd4-8f19-a64e5d661b2b";
+    String orgunitId = "cf774a3b-4910-4b16-b6b0-608f80d216a4";
+    BulkOperationResponse bulkOperationResponse = mbtClient.upsertSubscribersBulk(orgunitId, communicationList.id, List.of(crmContact));
+    System.out.println(bulkOperationResponse);
+
+    bulkOperationResponse = mbtClient.upsertContactsBulk(orgunitId, List.of(crmContact));
+    System.out.println(bulkOperationResponse);
+
   }
 }
