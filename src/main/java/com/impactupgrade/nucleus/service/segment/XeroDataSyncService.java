3f7fa4dad4fd35478c56ae518eb57612a880d8b9
vsydor <vs.nr.ua@gmail.com>
2024-09-24 01:08:17 +0300

    21368: Getting contacts for donations before donations sync

--- src/main/java/com/impactupgrade/nucleus/service/segment/XeroDataSyncService.java
+++ src/main/java/com/impactupgrade/nucleus/service/segment/XeroDataSyncService.java
@@ -1,13 +1,19 @@
 package com.impactupgrade.nucleus.service.segment;
 
 import com.impactupgrade.nucleus.environment.Environment;
+import com.impactupgrade.nucleus.model.AccountingContact;
 import com.impactupgrade.nucleus.model.AccountingTransaction;
 import com.impactupgrade.nucleus.model.CrmContact;
 import com.impactupgrade.nucleus.model.CrmDonation;
 import com.impactupgrade.nucleus.model.PagedResults;
 
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 public class XeroDataSyncService implements DataSyncService {
 
@@ -34,35 +40,50 @@ public class XeroDataSyncService implements DataSyncService {
   //TODO: move to abstract class?
   @Override
   public void syncContacts(Calendar updatedAfter) throws Exception {
-    PagedResults<CrmContact> contactPagedResults = env.primaryCrmService().getDonorContacts(updatedAfter);
-    for (PagedResults.ResultSet<CrmContact> resultSet : contactPagedResults.getResultSets()) {
-      if (env.accountingPlatformService().isPresent()) {
+    if (env.accountingPlatformService().isPresent()) {
+      PagedResults<CrmContact> contactPagedResults = env.primaryCrmService().getDonorContacts(updatedAfter);
+      for (PagedResults.ResultSet<CrmContact> resultSet : contactPagedResults.getResultSets()) {
         try {
           env.accountingPlatformService().get().updateOrCreateContacts(resultSet.getRecords());
         } catch (Exception e) {
           env.logJobError("{}/syncContacts failed: {}", this.name(), e);
         }
       }
+    } else {
+      env.logJobWarn("Accounting Platform Service is not defined!");
     }
   }
 
   @Override
   public void syncTransactions(Calendar updatedAfter) throws Exception {
-    List<CrmDonation> crmDonations = env.primaryCrmService().getDonations(updatedAfter);
     if (env.accountingPlatformService().isPresent()) {
-      //TODO: get all contacts ids for crm contacts and do bulk update instead?
+      List<CrmDonation> crmDonations = env.primaryCrmService().getDonations(updatedAfter);
+      if (crmDonations.isEmpty()) {
+        return;
+      }
+
+      // Get existing contacts first
+      List<CrmContact> crmContacts = getCrmContacts(crmDonations);
+      List<AccountingContact> accountingContacts = getAccountingContacts(crmContacts);
+      // and keep in a map
+      Map<String, AccountingContact> accountingContactMap = accountingContacts.stream()
+          .collect(Collectors.toMap(ac -> ac.crmContactId, ac -> ac));
+
+      // Then sync donations, after back-filling contact ids
+      List<AccountingTransaction> accountingTransactions = crmDonations.stream()
+          .map(crmDonation -> {
+            CrmContact crmContact = crmDonation.contact;
+            AccountingContact accountingContact = accountingContactMap.get(crmContact.id);
+            return toAccountingTransaction(accountingContact.contactId, crmContact, crmDonation);
+          })
+          .toList();
       try {
-        for (CrmDonation crmDonation : crmDonations) {
-          CrmContact crmContact = crmDonation.contact;
-          //TODO: replace with 'getContactIdForTransaction?'
-          String contactId = env.accountingPlatformService().get().updateOrCreateContacts(List.of(crmContact))
-              .stream()
-              .findFirst().orElse(null);
-          env.accountingPlatformService().get().createTransaction(toAccountingTransaction(contactId, crmContact, crmDonation));
-        }
+        env.accountingPlatformService().get().updateOrCreateTransactions(accountingTransactions);
       } catch (Exception e) {
         env.logJobError("{}/syncTransactions failed: {}", this.name(), e);
       }
+    } else {
+      env.logJobWarn("Accounting Platform Service is not defined!");
     }
   }
 
@@ -77,5 +98,22 @@ public class XeroDataSyncService implements DataSyncService {
         crmDonation.gatewayName,
         crmDonation.transactionId,
         crmDonation.isRecurring());
+  }
+
+  private List<CrmContact> getCrmContacts(List<CrmDonation> crmDonations) {
+    Set<String> crmContactIds = new HashSet<>();
+    return crmDonations.stream()
+        .map(crmDonation -> crmDonation.contact)
+        // Only unique ids
+        .filter(contact -> crmContactIds.add(contact.id))
+        .toList();
+  }
+
+  private List<AccountingContact> getAccountingContacts(List<CrmContact> crmContacts) throws Exception {
+    List<AccountingContact> accountingContacts = new ArrayList<>();
+    for (CrmContact crmContact : crmContacts) {
+      env.accountingPlatformService().get().getContact(crmContact).ifPresent(accountingContacts::add);
+    }
+    return accountingContacts;
   }
 }
