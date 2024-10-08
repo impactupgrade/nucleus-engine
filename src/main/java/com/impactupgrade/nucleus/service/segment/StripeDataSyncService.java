0e19a21fc047beafe1e437858c269fdf40fecdaa
vsydor <vs.nr.ua@gmail.com>
2024-10-08 23:51:10 +0300

    21371: nightly sync of updated contacts to Stripe (both AU and US)

--- src/main/java/com/impactupgrade/nucleus/service/segment/StripeDataSyncService.java
+++ src/main/java/com/impactupgrade/nucleus/service/segment/StripeDataSyncService.java
@@ -1,9 +1,12 @@
 package com.impactupgrade.nucleus.service.segment;
 
 import com.impactupgrade.nucleus.environment.Environment;
+import com.impactupgrade.nucleus.model.CrmAddress;
 import com.impactupgrade.nucleus.model.CrmContact;
 import com.impactupgrade.nucleus.model.CrmDonation;
 import com.impactupgrade.nucleus.model.PagedResults;
+import com.stripe.model.Customer;
+import com.stripe.param.CustomerUpdateParams;
 
 import java.util.Calendar;
 import java.util.List;
@@ -31,9 +34,15 @@ public class StripeDataSyncService implements DataSyncService {
   public void syncContacts(Calendar updatedAfter) throws Exception {
     PagedResults<CrmContact> contactPagedResults = env.primaryCrmService().getDonorIndividualContacts(updatedAfter);
     for (PagedResults.ResultSet<CrmContact> resultSet : contactPagedResults.getResultSets()) {
-      //TODO: bulk update?
+      if (resultSet.getRecords().isEmpty()) continue;
+
       for (CrmContact crmContact : resultSet.getRecords()) {
-        //TODO: update contact in Stripe
+        try {
+          //TODO: consider handling retry since e do 1 by 1?
+          updateCustomer(crmContact);
+        } catch (Exception e) {
+          env.logJobError("{}/syncContacts failed: {}", this.name(), e);
+        }
       }
     }
   }
@@ -44,5 +53,30 @@ public class StripeDataSyncService implements DataSyncService {
     for (CrmDonation crmDonation : crmDonations) {
       //TODO: bulk update in Stripe
     }
+  }
+
+  protected Customer updateCustomer(CrmContact crmContact) throws Exception {
+    String customerId = getCustomerId(crmContact);
+    Customer customer = env.stripeClient().getCustomer(customerId);
+    CustomerUpdateParams customerUpdateParams = CustomerUpdateParams.builder()
+        .setName(crmContact.getFullName())
+        .setEmail(crmContact.email)
+        .setAddress(toAddress(crmContact.mailingAddress))
+        .build();
+    return env.stripeClient().updateCustomer(customer, customerUpdateParams);
+  }
+
+  protected String getCustomerId(CrmContact crmContact) {
+    return crmContact.id; //TODO:
+  }
+
+  protected CustomerUpdateParams.Address toAddress(CrmAddress crmAddress) {
+    return CustomerUpdateParams.Address.builder()
+        .setLine1(crmAddress.street)
+        .setCity(crmAddress.city)
+        .setState(crmAddress.state)
+        .setPostalCode(crmAddress.postalCode)
+        .setCountry(crmAddress.country)
+        .build();
   }
 }
