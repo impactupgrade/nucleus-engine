62c377d0a1726c981a9663cf5956dd8c5557958b
Brett Meyer <brett@3riverdev.com>
2024-12-20 16:43:03 -0500

    StripeWebhookUtil refactoring [sc-23564]

--- src/main/java/com/impactupgrade/nucleus/util/StripeWebhookUtil.java
+++ src/main/java/com/impactupgrade/nucleus/util/StripeWebhookUtil.java
@@ -4,7 +4,6 @@
 
 package com.impactupgrade.nucleus.util;
 
-import com.google.common.base.Strings;
 import com.impactupgrade.nucleus.client.StripeClient;
 import com.impactupgrade.nucleus.environment.Environment;
 import com.stripe.model.WebhookEndpoint;
@@ -15,51 +14,51 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-// TODO: This is a start of what could become Portal endpoints for auto-provisioning Stripe.
-
 /**
  * Examples:
  *
  * CUSTOM INSTANCE
- * StripeWebhookUtil.setupWebhook(env, "axis-nucleus.herokuapp.com");
+ * StripeWebhookUtil.setupWebhook(env, "org-nucleus.herokuapp.com");
+ *
+ * CUSTOM INSTANCE WITH UNIQUE PATH
+ * StripeWebhookUtil.setupWebhook(env, "org-nucleus.herokuapp.com", "/api/stripe/webhook/unique-path");
  *
  * NUCLEUS CORE
- * StripeWebhookUtil.setupWebhook(env, "nucleus.impactupgrade.com", "8017d001-531e-4094-8757-8a251fc80652");
+ * StripeWebhookUtil.setupWebhookNucleusCore(env, "dc992cd2-a48e-4179-a82b-2b0f91dd879b");
  */
 public class StripeWebhookUtil {
 
   private static final Logger log = LogManager.getLogger(StripeWebhookUtil.class.getName());
 
   public static void setupWebhook(Environment env, String hostname) {
-    setupWebhook(env, hostname, "");
+    setupWebhook(env, hostname, "/api/stripe/webhook");
   }
 
-  public static void setupWebhook(Environment env, String hostname, String nucleusApiKey) {
+  public static void setupWebhookNucleusCore(Environment env, String nucleusApiKey) {
+    setupWebhook(env, "nucleus.impactupgrade.com", "/api/stripe/webhook?Nucleus-Api-Key=" + nucleusApiKey);
+  }
+
+  public static void setupWebhook(Environment env, String hostname, String path) {
     try {
       StripeClient stripeClient = new StripeClient(env);
 
       List<String> enabledEvents = List.of(
-        "charge.failed",
-        "charge.refunded",
-        "charge.refund.updated",
-        "charge.succeeded",
-        "charge.updated",
-        "customer.source.expiring",
-        "customer.subscription.created",
-        "customer.subscription.deleted",
-        "customer.subscription.updated",
-        "payment_intent.succeeded",
-        "payment_intent.payment_failed",
-        "payout.paid"
+          "charge.failed",
+          "charge.refunded",
+          "charge.refund.updated",
+          "charge.succeeded",
+          "charge.updated",
+          "customer.source.expiring",
+          "customer.subscription.created",
+          "customer.subscription.deleted",
+          "customer.subscription.updated",
+          "payment_intent.succeeded",
+          "payment_intent.payment_failed",
+          "payout.paid"
       );
 
-      String url = "https://" + hostname + "/api/stripe/webhook";
-      if (!Strings.isNullOrEmpty(nucleusApiKey)) {
-        url += "?Nucleus-Api-Key=" + nucleusApiKey;
-      }
-
       Map<String, Object> params = new HashMap<>();
-      params.put("url", url);
+      params.put("url", "https://" + hostname + path);
       params.put("enabled_events", enabledEvents);
       params.put("api_version", "2020-08-27");
 
